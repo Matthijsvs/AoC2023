d3d13a2f67de86cd0cc0d1b5218922694ab9ca48
Matthijsvs <46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com>
2023-12-16 13:27:22 +0100

    day 16 working

--- day_16.py
+++ day_16.py
@@ -1,137 +1,117 @@
-from PIL import Image, ImageDraw
-sample = """.|...\....
+import copy
+
+sample = r""".|...\....
 |.-.\.....
 .....|-...
 ........|.
 ..........
-.........\\
-..../.\\\\..
+.........\
+..../.\\..
 .-.-/..|..
-.|....-|.\\
+.|....-|.\
 ..//.|...."""
 
 
-grid = []
-colors = []
+def walk(x, y, dir, grid,colors):
+    step = 0
+    directions = [(1, 0),  # 0 east
+                  (0, 1),  # 1 south
+                  (-1, 0),  # 2 west
+                  (0, -1)]  # 3 north
+    newdir = {"-": [0, -1, 2, -1],
+              "|": [-1, 1, -1, 3],
+              ".": [0, 1, 2, 3],
+              "/": [3, 2, 1, 0],
+              "\\": [1, 0, 3, 2]}
 
-def bslash(x,y,im):
-    shape = [(x*4, y*4), ((x+1)*4, (y+1)*4)]
-    im.line(shape, fill ="black", width = 0)
-def slash(x,y,im):
-    shape = [((x+1)*4, y*4), (x*4, (y+1)*4)]
-    im.line(shape, fill ="black", width = 0)
-
-def beam2(x, y, dir,im):
-    steps=0
-    while 0 <= x < len(grid[0]) and 0 <= y < len(grid) and steps<100:  # loop until we're back
-        steps += 1
-        c = grid[y][x]
+    done = False
+    faker = None
+    while not done:
         colors[y][x] = "x"
+        step += 1
 
-        directions = [(1, 0),  # 0 east
-                      (0, 1),  # 1 south
-                      (-1, 0),  # 2 west
-                      (0, -1)]  # 3 north
-
-        u, v = directions[dir]
-        shape = [(x*4+2,y*4+2),(x*4+2+-2*u,y*4+2+-2*v)]
-        #im.rectangle((x*4, y*4, (x+1)*4, (y+1)*4), outline=(0, 0, 255))
-        im.line(shape, fill="red", width=0)
-
-        newdir = {"-":[0,-1,2,-1],
-                  "|":[-1,1,-1,3],
-                  ".":[0,1,2,3],
-                  "/": [3, 2, 1, 0],
-                  "\\": [1, 0, 3, 2]}
-
-
-        if ((dir == 1) or (dir == 3)) and (c == "-"):
-            shape = [(x * 4 , y * 4 + 2), (x * 4 + 4 , y * 4 + 2)]
-            im.line(shape, fill="black", width=0)
-            grid[y][x] = "?"
-            xa = x + directions[0][0]
-            ya = y + directions[0][1]
-            if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
-                beam2(xa, ya, 0,im)
-
-            xa = x + directions[2][0]
-            ya = y + directions[2][1]
-            if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
-                beam2(xa, ya, 2,im)
-            break
-        elif ((dir == 0) or (dir == 2)) and (c == "|"):
-            grid[y][x] = "?"
-            shape = [(x * 4 +2, y * 4 ), (x * 4 +2, y * 4 + 4)]
-            im.line(shape, fill="black", width=0)
-            xa = x + directions[1][0]
-            ya = y + directions[1][1]
-            if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
-                beam2(xa, ya, 1, im)
-            xa = x + directions[3][0]
-            ya = y + directions[3][1]
-            if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
-                beam2(xa, ya, 3,im)
-            break
-        elif c in newdir:
-            next_step = newdir[c][dir]
-            if next_step == -1:
-                raise AssertionError
-            xa = x + directions[next_step][0]
-            ya = y + directions[next_step][1]
-
-            if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
-                u, v = directions[next_step]
-                shape = [(x * 4 + 2, y * 4 + 2), (x * 4 + 2 + (2 * u), y * 4 + 2 + (2 * v))]
-                im.line(shape, fill="green", width=0)
-                dir = next_step
-                x = xa
-                y = ya
-                if grid[y][x]=="?":
-                    break
-            else:
-                print("Outside!")
-                break
-        elif c == "?":
-            print("end found??")
-            break
+        if faker:
+            c = faker
+            faker = None
         else:
-            #print(f"else situation {x},{y} --> {xa},{ya}",c,dir)
+            c = grid[y][x]
+        if c == ".":  # continue
+            a, b = directions[dir]
+            x += a
+            y += b
+            if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
+                break
+            continue
+        elif c == "/" or c == "\\":  # divert
+            j = newdir[c][dir]
+            if j == -1:
+                raise AssertionError
+            dir = j
+            faker = "."
+            continue
+        elif c == "-":  # split
+            if dir == 1 or dir == 3:
+                grid[y][x] = "?"
+                xa = x + directions[2][0]
+                ya = y + directions[2][1]
+                if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
+                    walk(xa, ya, 2, grid,colors)
+                xa = x + directions[0][0]
+                ya = y + directions[0][1]
+                if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
+                    walk(xa, ya, 0, grid,colors)
+            else:
+                faker = "."  # skip next cycle
+        elif c == "|":  # split
+            if dir == 0 or dir == 2:
+                grid[y][x] = "?"
+                xa = x + directions[1][0]
+                ya = y + directions[1][1]
+                if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
+                    walk(xa, ya, 1, grid,colors)
+                xa = x + directions[3][0]
+                ya = y + directions[3][1]
+                if 0 <= xa < len(grid[0]) and 0 <= ya < len(grid):
+                    walk(xa, ya, 3, grid,colors)
+            else:
+                faker = "."  # skip next cycle
+        else:
+            done = True
 
-            break
-            raise IOError("got lost along the way")
-    #print("out of while loop",xa,ya,c,dir)
 
-with open("input16.txt","r") as f:
-    # for y in sample.splitlines():
-    for y in f.readlines():
-        grid.append(list(y.strip()))
-        colors.append([" "] * len(grid[0]))
+with open("input16.txt", "r") as f:
+    lines = f.readlines()
+#lines = sample.splitlines()
+initial_grid = list(map(lambda x: list(x.strip()), lines))
+occupied = []
+for i in list(map(lambda x: list(x.strip()), lines)):
+    occupied.append(["_"] * len(initial_grid[0]))
 
-with Image.new("RGBA", (len(grid[0]*4), len(grid)*4), (255, 255, 255, 255)) as im:
-    imageSizeW, imageSizeH = im.size
-    img1 = ImageDraw.Draw(im)
-    for x in range(len(grid[0])):
-        for y in range(len(grid)):
-            if grid[y][x] == "/":
-                slash(x,y,img1)
-            elif grid[y][x] == "\\":
-                bslash(x,y,img1)
-    beam2(0, 0, 0,img1)  # top, left, east
-    # for x in range(len(grid[0])):
-    #    for y in range(len(grid)):
-    #        if colors[j][i] == "x":
-    #            im.putpixel((i, j), (255, 0, 0, 255))
 
-im.save("day16.png", "PNG")
+def counting(x, y, direction):
+    grid2 = list(map(lambda x: list(x.strip()), lines))
+    empty_map = copy.deepcopy(occupied)
+    walk(x, y, direction, grid2, empty_map)
+    s = 0
+    for i in empty_map:
+        for j in i:
+            if j == "x":
+                s += 1
+    #for i in empty_map:
+    #    for j in i:
+    #        print(j, end="")
+    #    print("")
+    return s
 
-for i in grid:
-    for j in i:
-        print(j,end="")
-    print("")
-sum_a=0
-for i in colors:
-    sum_a+=i.count("x")
-    for j in i:
-        print(j,end="")
-    print("")
-print(sum_a)
+
+print("part A", counting(0, 0, 0))
+
+print([counting(x,0,1) for x in range(len(initial_grid[0]))])
+part_b = max([counting(x, 0, 1) for x in range(len(initial_grid[0]))])
+print(part_b)
+part_b = max([counting(x, len(initial_grid) - 1, 3) for x in range(len(initial_grid[0]))])
+print(part_b)
+part_b = max([counting(0,y,0) for y in range(len(initial_grid))])
+print(part_b)
+part_b = max([counting(len(initial_grid[0]) - 1, y, 2) for y in range(len(initial_grid))])
+print(part_b)
